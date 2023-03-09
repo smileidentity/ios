ecf8ee045600cfae3bcfb7baae3e8fa93f8b46f7
Jubril O <jubrylola@gmail.com>
2023-03-09 06:57:23 +0300

    Add missing Json keys

--- Sources/Networking/Models/EnhancedKyc.swift
+++ Sources/Networking/Models/EnhancedKyc.swift
@@ -3,6 +3,7 @@ import Foundation
 public struct EnhancedKycRequest: Codable {
     var country: String
     var idType: String
+    var idNumber: String
     var firstName: String
     var middleName: String
     var lastName: String
@@ -16,6 +17,24 @@ public struct EnhancedKycRequest: Codable {
     var timestamp = ""
     var signature = ""
 
+    enum CodingKeys: String, CodingKey {
+        case country
+        case idType = "id_type"
+        case idNumber = "id_number"
+        case firstName = "first_name"
+        case middleName = "middle_name"
+        case lastName = "last_name"
+        case dob
+        case phoneNumber = "phone_number"
+        case bankCode = "bank_code"
+        case partnerParams = "partner_params"
+        case partnerId = "partner_id"
+        case sourceSdk = "source_sdk"
+        case sourceSdkVersion = "source_sdk_version"
+        case timestamp
+        case signature
+    }
+
 }
 
 public struct EnhancedKycResponse: Codable {
@@ -25,7 +44,7 @@ public struct EnhancedKycResponse: Codable {
     var resultCode: String
     var resultText: String
     var country: String
-    var actions: Actions
+    var actions: Actions?
     var idType: String
     var idNumber: String
     var fullName: String?
@@ -33,6 +52,23 @@ public struct EnhancedKycResponse: Codable {
     var dob: String?
     var base64Photo: String?
     var isFinalResult: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case smileJobId = "SmileJobID"
+        case partnerParams = "PartnerParams"
+        case resultType = "ResultType"
+        case resultCode = "ResultCode"
+        case resultText = "ResultText"
+        case country = "Country"
+        case actions = "Actions"
+        case idType = "IDType"
+        case idNumber = "IDNumber"
+        case fullName = "FullName"
+        case expirationDate = "ExpirationDate"
+        case dob = "DOB"
+        case base64Photo = "Photo"
+        case isFinalResult = "IsFinalResult"
+    }
 }
 
 enum IDType {
