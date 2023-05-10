7e75fdf8b779bfd243bf6270008b055693db3642
Jubril O <jubrylola@gmail.com>
2023-05-10 16:39:44 +0100

    Present instructions screen in full screen

---
+++ Example/SmileIdentity/UserIDViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class UserIDViewController: UIViewController {
+
+    @IBOutlet weak var userIdField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func onContinueTap(_ sender: Any) {
+
+    }
+}
