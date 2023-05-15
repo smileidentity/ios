b634a314a4fc725004d810551ef4ee7375656829
Jubril O <jubrylola@gmail.com>
2023-05-15 12:35:18 +0100

    Fix linting errors and failing tests

---
+++ Sources/Helpers/SmileIDError.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+public enum SmileIDError: Error {
+    case jobStatusTimeOut
+}
+
+//extension SmileIDError {
+//    public var errorDescription: String? {
+//        switch self {
+//        case .jobStatusTimeOut:
+//            NSLocalizedString(<#T##key: String##String#>, comment: "Job status time out")
+//        }
+//    }
+//
+//}
