05f6f01e9fe130cec3468b9cfa879dd81dc905e5
Jubril O <jubrylola@gmail.com>
2023-05-15 15:59:25 +0100

    Add smart selfie authentication. Copy userid to clipboard.

--- Example/SmileIdentity/HomeViewController.swift
+++ Example/SmileIdentity/HomeViewController.swift
@@ -6,6 +6,8 @@ import SmileIdentity
 class HomeViewController: UIViewController, SmartSelfieResultDelegate {
     var cameraVC: UIViewController?
     var cancellable: AnyCancellable?
+    var userID = ""
+    var currentJob = JobType.smartSelfieEnrollment
 
     @IBOutlet var versionLabel: CopyableLabel!
     override func viewDidLoad() {
@@ -15,31 +17,61 @@ class HomeViewController: UIViewController, SmartSelfieResultDelegate {
     }
 
     @IBAction func onSmartSelfieRegistrationTap(_ sender: Any) {
-        let smartSelfieAuthenticationView = SmileIdentity.smartSelfieRegistrationScreen(delegate: self)
-        cameraVC = UIHostingController(rootView: smartSelfieAuthenticationView)
+        userID = UUID().uuidString
+        currentJob = .smartSelfieEnrollment
+        let smartSelfieRegistrationScreen = SmileIdentity.smartSelfieRegistrationScreen(userId: userID,
+                                                                                        delegate: self)
+        cameraVC = UIHostingController(rootView: smartSelfieRegistrationScreen)
         cameraVC?.modalPresentationStyle = .fullScreen
         navigationController?.present(cameraVC!, animated: true)
     }
 
     @IBAction func onSmartSelfieAuthenticationTap(_ sender: Any) {
+        currentJob = .smartSelfieAuthentication
         if let userIDController = storyboard?.instantiateViewController(withIdentifier: "UserIDViewController")
             as? UserIDViewController {
+            userIDController.userID = userID
+            userIDController.handleContinueTap = { [weak self] userid in
+                self?.smartSelfieAuthenticationScreen(userID: userid)
+            }
             navigationController?.pushViewController(userIDController, animated: true)
         }
     }
 
+    func smartSelfieAuthenticationScreen(userID: String) {
+        let smartSelfieAuthenticationScreen = SmileIdentity.smartSelfieAuthenticationScreen(userId: userID,
+                                                                                            delegate: self)
+        cameraVC = UIHostingController(rootView: smartSelfieAuthenticationScreen)
+        cameraVC?.modalPresentationStyle = .fullScreen
+        navigationController?.present(cameraVC!, animated: true)
+    }
+
     func didSucceed(selfieImage: Data, livenessImages: [Data], jobStatusResponse: JobStatusResponse) {
         cameraVC?.dismiss(animated: true, completion: {
-            let ac =
-            UIAlertController(title: "Selfie Capture Complete",
-                              message: "The Job has been submited. Check your Portal for the status of the job",
-                              preferredStyle: .alert)
-            ac.addAction(.init(title: "Okay", style: .default))
-            self.navigationController?.present(ac, animated: true)
+
+            switch self.currentJob {
+            case .smartSelfieEnrollment:
+                UIPasteboard.general.string = self.userID
+                self.presentAlert(title: "Smart Selfie Enrollment Complete",
+                                  message: "The user has been registered and the user id has been copied to the clipboard.")
+            case .smartSelfieAuthentication:
+                self.presentAlert(title: "Smart Selfie Authentication Complete",
+                                  message: "The user has been authenticated succesfully")
+            default:
+                break
+            }
         })
     }
 
+    func presentAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+        alertController.addAction(.init(title: "Okay", style: .default))
+        self.navigationController?.present(alertController, animated: true)
+    }
+
     func didError(error: Error) {
-        print("Error - \(error.localizedDescription)")
+        presentAlert(title: "An error occured", message: error.localizedDescription)
     }
 }
--- Example/SmileIdentity/UserIDViewController.swift
+++ Example/SmileIdentity/UserIDViewController.swift
@@ -1,14 +1,19 @@
+// swiftlint:disable force_cast
 import UIKit
 
 class UserIDViewController: UIViewController {
-
+    var userID = ""
     @IBOutlet weak var userIdField: UITextField!
+    var handleContinueTap: ((String) -> Void)?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        userIdField.text = userID
     }
 
     @IBAction func onContinueTap(_ sender: Any) {
-
+        if  !userIdField.text!.isEmpty {
+            handleContinueTap?(userIdField.text!)
+        }
     }
 }
