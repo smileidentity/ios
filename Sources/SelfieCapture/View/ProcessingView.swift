39994bc6ff47901933b05c07dd6277dcb3bcb39d
Jubril O <jubrylola@gmail.com>
2023-05-15 16:41:50 +0100

    Add modal views

---
+++ Sources/SelfieCapture/View/ProcessingView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ProcessingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProcessingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProcessingView()
+    }
+}
