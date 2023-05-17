222400344750aba9a29f6f136c82064f7f0c933f
Jubril O <jubrylola@gmail.com>
2023-05-17 11:45:00 +0100

    Create selfie confirmation view

--- Sources/SelfieCapture/View/SelfieConfirmationView.swift
+++ Sources/SelfieCapture/View/SelfieConfirmationView.swift
@@ -1,13 +1,63 @@
 import SwiftUI
 
 struct SelfieConfirmationView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var image: UIImage
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            VStack(spacing: 16) {
+                Text(SmileIDResourcesHelper.localizedString(for: "Confirmation.GoodSelfie"))
+                    .multilineTextAlignment(.center)
+                    .font(SmileIdentity.theme.header2)
+                    .foregroundColor(SmileIdentity.theme.accent)
+
+                Text(SmileIDResourcesHelper.localizedString(for: "Confirmation.FaceClear"))
+                    .multilineTextAlignment(.center)
+                    .font(SmileIdentity.theme.header5)
+                    .foregroundColor(SmileIdentity.theme.tertiary)
+                    .lineSpacing(1.3)
+            }
+            VStack {
+                Image(uiImage: image)
+            }
+
+            VStack {
+                SmileButton(style: .secondary,
+                            title: "Confirmation.YesUse",
+                            clicked: {})
+                SmileButton(style: .secondary,
+                            title: "Confirmation.Retake",
+                            clicked: {presentationMode.wrappedValue.dismiss()})
+            }
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: 20)
     }
 }
 
-struct SelfieConfirmationView_Previews: PreviewProvider {
+struct ContentView: View {
+    @State private var showModal = false
+
+    var body: some View {
+        ZStack {
+            Button(action: {
+                withAnimation {
+                    showModal.toggle()
+                }
+            }) {
+                Text("Show Modal")
+            }
+            ModalPresenter(isPresented: $showModal) {
+                SelfieConfirmationView(image: UIImage())
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        SelfieConfirmationView()
+        ContentView()
     }
 }
