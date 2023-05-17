76a0bb6992aca47b7d4813f15cf8c311b732516b
Jubril O <jubrylola@gmail.com>
2023-05-17 13:49:02 +0100

    Add image processing UI

--- Sources/SelfieCapture/View/ProcessingView.swift
+++ Sources/SelfieCapture/View/ProcessingView.swift
@@ -2,7 +2,30 @@ import SwiftUI
 
 struct ProcessingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        VStack(spacing: 20) {
+            InfiniteProgressBar()
+                .frame(width: 60)
+            Image(uiImage: SmileIDResourcesHelper.FaceOutline)
+            VStack(spacing: 16) {
+                Text(SmileIDResourcesHelper.localizedString(for: "Confirmation.Processing"))
+                    .multilineTextAlignment(.center)
+                    .font(SmileIdentity.theme.header4)
+                    .foregroundColor(SmileIdentity.theme.accent)
+
+                Text(SmileIDResourcesHelper.localizedString(for: "Confirmation.Time"))
+                    .multilineTextAlignment(.center)
+                    .font(SmileIdentity.theme.header5)
+                    .foregroundColor(SmileIdentity.theme.tertiary)
+                .lineSpacing(1.3)
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.bottom, 80)
+        }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(radius: 20)
     }
 }
 
