8edd0a9f9d9a0caacb53ff41a224c8581d92ee2a
Jubril O <jubrylola@gmail.com>
2023-05-24 12:41:31 +0100

    Update sample app UI to use swift ui

---
+++ Example/SmileIdentity/AboutUsView.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+import SmileIdentity
+
+struct AboutUsView: View {
+
+    init() {
+        UITableView.appearance().backgroundColor = offWhiteUIColor
+        UITableViewCell.appearance().backgroundColor = offWhiteUIColor
+    }
+    var body: some View {
+        NavigationView {
+            List {
+                HStack(spacing: 10) {
+                    Image(systemName: "info.circle.fill")
+                    Text("Who we are")
+                }
+                    Button(action: {
+                        if let url = URL(string: "https://www.mylink.com") {
+                            UIApplication.shared.open(url)
+                        }
+                    }, label: {
+                        HStack(spacing: 10) {
+                            Image(systemName: "star.fill")
+                            Text("Visit our website")
+                        }
+                    })
+
+                Button(action: {
+                    if let url = URL(string: "https://www.mylink.com") {
+                        UIApplication.shared.open(url)
+                    }
+                }, label: {
+                    HStack(spacing: 10) {
+                        Image(systemName: "envelope.fill")
+                        Text("Contact support")
+                    }
+                })
+
+            }.font(SmileIdentity.theme.body)
+                .background(offWhite)
+                .navigationBarTitle("Smile ID", displayMode: .inline)
+                .navigationBarColor(sand)
+        }
+    }
+}
+
+struct AboutUsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutUsView()
+    }
+}
