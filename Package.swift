bc097b98c5eff429f0bfb9ff1b838aaf444eb86f
Japhet <jndhlovu54@gmail.com>
2023-05-24 20:58:21 +0200

    github action code review

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/marmelroy/Zip", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/marmelroy/Zip", .upToNextMajor(from: "2.1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
