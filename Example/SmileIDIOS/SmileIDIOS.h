c97dd64e78be3f79c3cbf5c42a0f7cb6b8dbaf9d
Japhet <jndhlovu54@gmail.com>
2023-05-24 20:45:23 +0200

    ios sdk build actions

---
+++ Example/SmileIDIOS/SmileIDIOS.h
@@ -0,0 +1,19 @@
+//
+//  SmileIDIOS.h
+//  SmileIDIOS
+//
+//  Created by Japhet Ndhlovu on 2023/05/24.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SmileIDIOS.
+FOUNDATION_EXPORT double SmileIDIOSVersionNumber;
+
+//! Project version string for SmileIDIOS.
+FOUNDATION_EXPORT const unsigned char SmileIDIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmileIDIOS/PublicHeader.h>
+
+
