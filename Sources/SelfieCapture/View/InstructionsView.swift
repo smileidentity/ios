7b8cf29dbdcfb11ca6bf930f1e22dafd7969f0c7
Jubril O <jubrylola@gmail.com>
2023-06-16 14:53:55 +0100

    Improve instructions view directive logic

--- Sources/SelfieCapture/View/InstructionsView.swift
+++ Sources/SelfieCapture/View/InstructionsView.swift
@@ -3,51 +3,17 @@ import SwiftUI
 struct InstructionsView: View {
     @ObservedObject private(set) var model: SelfieCaptureViewModel
     @State private var debounceTimer: Timer?
-    @State private var directive: LocalizedStringKey = "Instructions.Start"
+    @State private var directive: String = "Instructions.Start"
     var body: some View {
-        Text(SmileIDResourcesHelper.localizedString(for: directive.stringKey))
+        Text(SmileIDResourcesHelper.localizedString(for: directive))
             .multilineTextAlignment(.center)
             .foregroundColor(SmileID.theme.accent)
             .font(SmileID.theme.header4)
             .frame(maxWidth: 300)
             .transition(.slide)
-            .onReceive(model.$faceDetectionState, perform: {_ in
-                faceDetectionState()
+            .onReceive(model.$directive, perform: { value in
+                directive = value
             })
-    }
-}
-
-extension InstructionsView {
-    func faceDetectionState() {
-        debounceTimer?.invalidate()
-        debounceTimer = Timer.scheduledTimer(withTimeInterval: 0, repeats: false) { _ in
-            switch model.faceDetectionState {
-            case .smileFrame:
-                directive = "Instructions.Smile"
-            case .sceneUnstable:
-                directive = "Instructions.Unstable"
-            case .faceDetectionErrored:
-                directive = "Instructions.UnknownError"
-            case .noFaceDetected:
-                directive = "Instructions.Start"
-            case .faceDetected:
-                if model.hasDetectedValidFace && model.faceDetectionState == .smileFrame {
-                    directive = "Instructions.Smile"
-                } else if model.isAcceptableBounds == .detectedFaceOffCentre {
-                    directive = "Instructions.UnableToDetectFace"
-                } else if model.isAcceptableBounds == .detectedFaceTooSmall {
-                    directive = "Instructions.FaceFar"
-                } else if model.isAcceptableBounds == .detectedFaceTooLarge {
-                    directive = "Instructions.FaceClose"
-                } else {
-                    directive = "Instructions.Capturing"
-                }
-            case .multipleFacesDetected:
-                directive = "Instructions.MultipleFaces"
-            case .finalFrame:
-                directive = "Instructions.Smile"
-            }
-        }
     }
 }
 
