88286d4e69cd1e379e7c52f36517599c563b8948
Japhet <jndhlovu54@gmail.com>
2023-07-12 14:38:51 +0200

    fix:uiview swiftlin

--- Sources/SmileID/Classes/Views/SmileUIView.swift
+++ Sources/SmileID/Classes/Views/SmileUIView.swift
@@ -3,11 +3,11 @@ import SwiftUI
 public struct SmileUIView<Content: View>: View {
     @EnvironmentObject var navigation: NavigationHelper
     let content: Content
-    
+
     init(@ViewBuilder _ content: () -> Content) {
         self.content = content()
     }
-    
+
     public var body: some View {
         NavigationView {
             content
