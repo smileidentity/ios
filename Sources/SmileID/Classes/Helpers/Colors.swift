16e051947bcec508ffdb3bc7f61f142bfb836c04
Jubril O <jubrylola@gmail.com>
2023-07-18 05:11:25 +0100

    Fix more linting issues

--- Sources/SmileID/Classes/Helpers/Colors.swift
+++ Sources/SmileID/Classes/Helpers/Colors.swift
@@ -25,4 +25,40 @@ extension Color {
             opacity: Double(alpha) / 255
         )
     }
+
+    func uiColor() -> UIColor {
+
+        if #available(iOS 14.0, *) {
+            return UIColor(self)
+        }
+
+        let components = self.components()
+        return UIColor(red: components.red, green: components.green, blue: components.blue, alpha: components.alpha)
+    }
+
+    func components() -> ColorComponent {
+
+        let scanner = Scanner(string: self.description.trimmingCharacters(in: CharacterSet.alphanumerics.inverted))
+        var hexNumber: UInt64 = 0
+        var r: CGFloat = 0.0, g: CGFloat = 0.0, b: CGFloat = 0.0, a: CGFloat = 0.0
+
+        let result = scanner.scanHexInt64(&hexNumber)
+        if result {
+            r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+            g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+            b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+            a = CGFloat(hexNumber & 0x000000ff) / 255
+        }
+        return ColorComponent(red: r,
+                              green: g,
+                              blue: b,
+                              alpha: a)
+    }
+}
+
+struct ColorComponent {
+    var red: Double
+    var green: Double
+    var blue: Double
+    var alpha: Double
 }
