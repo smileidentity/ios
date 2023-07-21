8bf56b89e3927e2ab4041023f2c50aa50953632a
Vansh Gandhi <vansh@smileidentity.com>
2023-07-21 14:34:15 -0700

    Bump version

--- Sources/SmileID/Classes/SmileID.swift
+++ Sources/SmileID/Classes/SmileID.swift
@@ -22,7 +22,7 @@ public class SmileID {
     }()
 
     private init() {}
-    public static let version = "10.0.0-beta01"
+    public static let version = "10.0.0-beta02"
     internal static var config: Config!
     internal static var useSandbox = true
     public private(set) static var theme: SmileIdTheme = DefaultTheme()
