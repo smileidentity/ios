8b6529d0bc1de8785ce167e310efb18be973095f
Jubril O <jubrylola@gmail.com>
2023-07-21 23:15:14 +0100

    Add confirmation view

--- Sources/SmileID/Classes/DocumentVerification/View/DocumentConfirmationView.swift
+++ Sources/SmileID/Classes/DocumentVerification/View/DocumentConfirmationView.swift
@@ -2,7 +2,31 @@ import SwiftUI
 
 struct DocumentConfirmationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 49) {
+            VStack(spacing: 16) {
+                Text("Hello, World!")
+                    .multilineTextAlignment(.center)
+                    .font(SmileID.theme.header1)
+                    .foregroundColor(SmileID.theme.accent)
+                    .lineSpacing(0.98)
+                Text("callout")
+                    .multilineTextAlignment(.center)
+                    .font(SmileID.theme.header5)
+                    .foregroundColor(SmileID.theme.tertiary)
+                    .lineSpacing(1.3)
+            }
+
+            Image("")
+
+            VStack(spacing: 16) {
+                SmileButton(style: .secondary,
+                            title: "button1",
+                            clicked: {})
+                SmileButton(style: .secondary,
+                            title: "button1",
+                            clicked: {})
+            }
+        }
     }
 }
 
