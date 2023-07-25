ae207b9202e860428e714999bcc1189f7bdbc2b1
Japhet <jndhlovu54@gmail.com>
2023-07-25 15:52:03 +0200

    fix:lint

--- Sources/SmileID/Classes/SmileID.swift
+++ Sources/SmileID/Classes/SmileID.swift
@@ -1,17 +1,17 @@
 import Foundation
-import UIKit
 import SwiftUI
+import UIKit
 
 public class SmileID {
     @Injected var injectedApi: SmileIDServiceable
     public static var api: SmileIDServiceable {
         return SmileID.instance.injectedApi
     }
-    
+
     public static var configuration: Config {
         return config
     }
-    
+
     internal static let instance: SmileID = {
         let container = DependencyContainer.shared
         container.register(SmileIDServiceable.self) { SmileIDService() }
@@ -20,37 +20,38 @@ public class SmileID {
         let instance = SmileID()
         return instance
     }()
-    
+
     private init() {}
     public static let version = "10.0.0-beta02"
     internal static var config: Config!
     internal static var useSandbox = true
     internal static let navigation = NavigationHelper()
     public private(set) static var theme: SmileIdTheme = DefaultTheme()
-    
+
     public class func initialize(config: Config, useSandbox: Bool = true) {
         self.config = config
         self.useSandbox = useSandbox
         SmileIDResourcesHelper.registerFonts()
     }
-    
+
     public class func apply(_ theme: SmileIdTheme) {
         self.theme = theme
     }
-    
+
     public class func smartSelfieEnrollmentScreen(userId: String = "user-\(UUID().uuidString)",
                                                   jobId: String = "job-\(UUID().uuidString)",
                                                   allowAgentMode: Bool = false,
                                                   showAttribution: Bool = true,
                                                   showInstruction: Bool = true,
                                                   delegate: SmartSelfieResultDelegate)
-    -> some View {
+        -> some View
+    {
         let viewModel = SelfieCaptureViewModel(userId: userId,
                                                jobId: jobId,
                                                isEnroll: true,
                                                allowsAgentMode: allowAgentMode,
                                                showAttribution: showAttribution)
-        showInstruction ? navigation.navigate(to: .selfieInstructionScreen) : navigation.navigate(to: . selfieCaptureScreen)
+        showInstruction ? navigation.navigate(to: .selfieInstructionScreen) : navigation.navigate(to: .selfieCaptureScreen)
         return SmileUIView {
             if showInstruction {
                 SmartSelfieInstructionsView(viewModel: viewModel, delegate: delegate)
@@ -59,33 +60,35 @@ public class SmileID {
             }
         }.environmentObject(navigation)
     }
-    
+
     public class func documentVerificationScreen(userId _: String = "user-\(UUID().uuidString)",
                                                  jobId _: String = "job-\(UUID().uuidString)",
-                                                 showAttribution: Bool = true,
+                                                 showAttribution _: Bool = true,
                                                  showInstruction: Bool = true,
                                                  delegate: DocumentCaptureResultDelegate)
-    -> some View {
-        showInstruction ? navigation.navigate(to: .documentCaptureInstructionScreen) : navigation.navigate(to: . documentCaptureScreen)
+        -> some View
+    {
+        showInstruction ? navigation.navigate(to: .documentCaptureInstructionScreen) : navigation.navigate(to: .documentCaptureScreen)
         let viewModel = DocumentCaptureViewModel()
         return SmileUIView {
             if showInstruction {
                 DocumentCaptureInstructionsView(viewModel: viewModel, delegate: delegate)
             } else {
-                //todo: integrate with Jubril's view here
+                // TODO: integrate with Jubril's view here
                 DocumentCaptureInstructionsView(viewModel: viewModel, delegate: delegate)
             }
         }.environmentObject(navigation)
     }
-    
+
     public class func smartSelfieAuthenticationScreen(userId: String,
                                                       jobId: String = "job-\(UUID().uuidString)",
                                                       allowAgentMode: Bool = false,
                                                       showAttribution: Bool = true,
                                                       showInstruction: Bool = true,
                                                       delegate: SmartSelfieResultDelegate)
-    -> some View {
-        showInstruction ? navigation.navigate(to: .selfieInstructionScreen) : navigation.navigate(to: . selfieCaptureScreen)
+        -> some View
+    {
+        showInstruction ? navigation.navigate(to: .selfieInstructionScreen) : navigation.navigate(to: .selfieCaptureScreen)
         let viewModel = SelfieCaptureViewModel(userId: userId,
                                                jobId: jobId,
                                                isEnroll: false,
@@ -99,7 +102,7 @@ public class SmileID {
             }
         }.environmentObject(navigation)
     }
-    
+
     public class func setEnvironment(useSandbox: Bool) {
         SmileID.useSandbox = useSandbox
     }
