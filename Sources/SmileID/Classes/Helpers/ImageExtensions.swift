9eff47fb061fcc7e16af25628b08d2a60078ff8b
Jubril O <jubrylola@gmail.com>
2023-08-14 12:41:24 +0100

    Add image orientation extensions

--- Sources/SmileID/Classes/Helpers/ImageExtensions.swift
+++ Sources/SmileID/Classes/Helpers/ImageExtensions.swift
@@ -21,3 +21,40 @@ extension Image {
 #endif
     }
 }
+
+extension CGImagePropertyOrientation {
+    init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+        case .up:
+            self = .up
+        case .upMirrored:
+            self = .upMirrored
+        case .down:
+            self = .down
+        case .downMirrored:
+            self = .downMirrored
+        case .left:
+            self = .left
+        case .leftMirrored:
+            self = .leftMirrored
+        case .right:
+            self = .right
+        case .rightMirrored:
+            self = .rightMirrored
+        @unknown default:
+            assertionFailure("Unknown orientation, falling to default")
+            self = .right
+        }
+    }
+}
+
+extension UIImage {
+    /// Creates a UIImage from the specified CIImage.
+    static func from(ciImage: CIImage) -> UIImage {
+        if let cgImage = CIContext(options: nil).createCGImage(ciImage, from: ciImage.extent) {
+            return UIImage(cgImage: cgImage)
+        } else {
+            return UIImage(ciImage: ciImage, scale: 1.0, orientation: .up)
+        }
+    }
+}
