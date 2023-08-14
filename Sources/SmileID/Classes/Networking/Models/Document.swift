dc64086a3d299b55067af6987e7c3ddd73b5fb7a
Jubril O <jubrylola@gmail.com>
2023-08-14 12:52:23 +0100

    Expose `Document` properties and init

--- Sources/SmileID/Classes/Networking/Models/Document.swift
+++ Sources/SmileID/Classes/Networking/Models/Document.swift
@@ -1,7 +1,15 @@
 import Foundation
 
-struct Document {
-    var countryCode: String
-    var documentType: String
-    var aspectRatio: Double
+public struct Document {
+    public var countryCode: String
+    public var documentType: String
+    public var aspectRatio: Double
+
+    public init(countryCode: String,
+                documentType: String,
+                aspectRatio: Double) {
+        self.countryCode = countryCode
+        self.documentType = documentType
+        self.aspectRatio = aspectRatio
+    }
 }
