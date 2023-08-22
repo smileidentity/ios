25e38b52e826b1c94311d8ab4b29b9665004a816
Jubril O <jubrylola@gmail.com>
2023-08-22 13:36:18 +0100

    Add `IdInfo` to upload request

--- Sources/SmileID/Classes/Networking/Models/UploadRequest.swift
+++ Sources/SmileID/Classes/Networking/Models/UploadRequest.swift
@@ -2,9 +2,35 @@ import Foundation
 
 public struct UploadRequest: Codable {
     var images: [UploadImageInfo]
+    var idInfo: IdInfo?
 
     enum CodingKeys: String, CodingKey {
         case images
+        case idInfo = "id_info"
+    }
+}
+
+public struct IdInfo: Codable {
+    var country: String
+    var idType: String
+    var idNumber: String?
+    var firstName: String?
+    var middleName: String?
+    var lastName: String?
+    var dob: String?
+    var bankCode: String?
+    var entered: Bool?
+
+    enum  CodingKeys: String, CodingKey {
+        case country
+        case idType = "id_type"
+        case idNumber = "id_number"
+        case firstName = "first_name"
+        case middleName = "middle_name"
+        case lastName = "last_name"
+        case dob
+        case bankCode = "bank_code"
+        case entered
     }
 }
 
