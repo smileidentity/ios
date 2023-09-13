5b899b4f6cc72af9121ab22f97a99eacd591cfe0
Jubril O <jubrylola@gmail.com>
2023-09-13 15:24:21 +0100

    Make SmileIDLocalizableStrings initializer public

--- Sources/SmileID/Classes/Helpers/SmileIDLocalizableStrings.swift
+++ Sources/SmileID/Classes/Helpers/SmileIDLocalizableStrings.swift
@@ -3,7 +3,12 @@ import Foundation
 /// Encapsulates the bundle and tablename of the Localizeable.strings file to be used within the SDK
 public struct SmileIDLocalizableStrings {
     /// The bundle where the localizable string is located
-    var bundle: Bundle
+    public var bundle: Bundle
     /// The name of the localizable strings file. Do not include the files `.strings` extension
-    var tablename: String
+    public var tablename: String
+
+    public init(bundle: Bundle, tablename: String) {
+        self.bundle = bundle
+        self.tablename = tablename
+    }
 }
