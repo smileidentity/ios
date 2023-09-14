cb4e0c00042bb1493a91d0d52896156c6f14a999
Jubril O <jubrylola@gmail.com>
2023-09-14 21:47:06 +0100

    fix syntax error

--- Sources/SmileID/Classes/DocumentVerification/Model/DocumentCaptureModel.swift
+++ Sources/SmileID/Classes/DocumentVerification/Model/DocumentCaptureModel.swift
@@ -392,7 +392,7 @@ class DocumentCaptureViewModel: ObservableObject, JobSubmittable, ConfirmationDi
                     self.borderColor = SmileID.theme.success.uiColor()
                     if self.autoCaptureTimer == nil {
                         self.showCaptureButton = false
-                        self.autoCaptureTimer = Timer.scheduledTimer(timeInterval: autoCaptureDelay,
+                        self.autoCaptureTimer = Timer.scheduledTimer(timeInterval: self.autoCaptureDelay,
                                                                      target: self,
                                                                      selector: #selector(self.captureImage),
                                                                      userInfo: nil,
