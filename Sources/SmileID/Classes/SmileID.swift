b1dd758b93bc71df9c6a76c10d2e31777399c8c1
Jubril Olambiwonnu <jubrylola@gmail.com>
2023-09-14 22:36:47 +0100

    Update Sources/SmileID/Classes/SmileID.swift
    Co-authored-by: Vansh Gandhi <vansh@smileidentity.com>

--- Sources/SmileID/Classes/SmileID.swift
+++ Sources/SmileID/Classes/SmileID.swift
@@ -35,8 +35,6 @@ public class SmileID {
     /// before calling any other SmileID methods.
     /// - Parameters:
     ///   - config: The smile config file. If no value is supplied, we check the app's main bundle for a `smile_config.json` file.
-    ///   - localizableStrings: A `SmileIDLocalizableStrings`  used to override all copy used within the SDK.
-    ///   if no value is set, the default copy will be used.
     ///   - useSandbox: A boolean to enable the sandbox environment or not
     public class func initialize(config: Config = try! Config(url: Bundle.main.url(forResource: "smile_config",
                                                                                    withExtension: "json")!),
