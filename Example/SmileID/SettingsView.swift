04c90493ae770c3588af05f9b60e0455660a2141
Vansh Gandhi <vansh@smileidentity.com>
2023-10-31 13:19:56 -0700

    Add error for invalid config

--- Example/SmileID/SettingsView.swift
+++ Example/SmileID/SettingsView.swift
@@ -2,7 +2,7 @@ import SmileID
 import SwiftUI
 
 struct SettingsView: View {
-    @State private var showSheet = false
+    @ObservedObject private var viewModel = SettingsViewModel()
 
     var body: some View {
         NavigationView {
@@ -10,22 +10,21 @@ struct SettingsView: View {
                 VStack(spacing: 16) {
                     SettingsCell(
                         imageName: "doc.badge.gearshape",
-                        title: "Update Smile Config"
-                    ) {
-                        print("Update Smile Config")
-                        showSheet.toggle()
-                    }
+                        title: "Update Smile Config",
+                        action: viewModel.onUpdateSmileConfigSelected
+                    )
                     Spacer()
                 }
-                    .sheet(isPresented: $showSheet) {
+                    .sheet(isPresented: $viewModel.showSheet) {
                         // Use a ZStack here so that the backgroundColor fills up the entire modal,
                         // otherwise some jarring white sections get left at the top and bottom
                         // https://stackoverflow.com/a/73561306
                         ZStack {
                             SmileID.theme.backgroundLightest.edgesIgnoringSafeArea(.all)
-                            let content = SmileConfigEntryView { newConfig in
-                                print("New Smile Config: \(newConfig)")
-                            }
+                            let content = SmileConfigEntryView(
+                                errorMessage: viewModel.errorMessage,
+                                onNewSmileConfig: viewModel.updateSmileConfig
+                            )
                             if #available(iOS 16.0, *) {
                                 content
                                     .presentationDetents([.medium])
@@ -71,7 +70,16 @@ private struct SettingsCell: View {
 }
 
 private struct SmileConfigEntryView: View {
-    let onNewSmileConfig: (_ newConfig: String) -> Void
+    private let errorMessage: String?
+    private let onNewSmileConfig: (_ newConfig: String) -> Void
+
+    init(
+        errorMessage: String? = nil,
+        onNewSmileConfig: @escaping (_ newConfig: String) -> Void
+    ) {
+        self.errorMessage = errorMessage
+        self.onNewSmileConfig = onNewSmileConfig
+    }
 
     @State private var smileConfigTextFieldValue = ""
     var body: some View {
@@ -97,14 +105,20 @@ private struct SmileConfigEntryView: View {
                         .lineLimit(10)
                 )
             }
+            let strokeColor = errorMessage == nil ? SmileID.theme.accent : SmileID.theme.error
             textField
                 .autocorrectionDisabled(true)
                 .foregroundColor(SmileID.theme.onLight)
                 .padding()
                 .overlay(
-                    RoundedRectangle(cornerRadius: 10).stroke(SmileID.theme.accent, lineWidth: 2)
+                    RoundedRectangle(cornerRadius: 10).stroke(strokeColor, lineWidth: 2)
                 )
                 .padding()
+            if let errorMessage = errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(SmileID.theme.error)
+                    .padding()
+            }
 
             Spacer()
 
@@ -144,5 +158,30 @@ private struct SmileConfigEntryView: View {
 private struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
+    }
+}
+
+class SettingsViewModel: ObservableObject {
+    @Published @MainActor var errorMessage: String?
+    @Published @MainActor var showSheet = false
+
+    private let jsonDecoder = JSONDecoder()
+
+    func onUpdateSmileConfigSelected() {
+        DispatchQueue.main.async { self.showSheet = true }
+    }
+
+    func updateSmileConfig(_ newConfig: String) {
+        do {
+            let config = try jsonDecoder.decode(Config.self, from: newConfig.data(using: .utf8)!)
+            SmileID.initialize(config: config)
+            DispatchQueue.main.async {
+                self.errorMessage = nil
+                self.showSheet = false
+            }
+        } catch {
+            print("Error decoding new config: \(error)")
+            DispatchQueue.main.async { self.errorMessage = "Invalid Config" }
+        }
     }
 }
