165b0df8bf9a86e6c3101c7ad331b3d7f7f54ede
Japhet Isheanesu Ndhlovu <JNdhlovu@users.noreply.github.com>
2023-12-13 22:26:40 +0200

    feat: release 10.0.1 (#108)
    * feat: release 10.0.1
    
    * fix: corrected tag
    
    * Update Podfile
    
    ---------
    
    Co-authored-by: Vansh Gandhi <vansh@smileidentity.com>

--- Sources/SmileID/Classes/SmileID.swift
+++ Sources/SmileID/Classes/SmileID.swift
@@ -3,7 +3,7 @@ import SwiftUI
 import UIKit
 
 public class SmileID {
-    public static let version = "10.0.0"
+    public static let version = "10.0.1"
     @Injected var injectedApi: SmileIDServiceable
     public static var configuration: Config { config }
 
