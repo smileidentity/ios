ad9d7c985cd06fe62bcaf92f1fba7da865ef5af8
Vansh Gandhi <vansh@smileidentity.com>
2023-12-14 10:54:23 -0800

    Fix white document overlay cutout (#109)
    * Fix white box
    
    * Update CHANGELOG and comments

--- Sources/SmileID/Classes/Util.swift
+++ Sources/SmileID/Classes/Util.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 public func generateJobId() -> String {
     generateId("job-")
@@ -10,4 +11,27 @@ public func generateUserId() -> String {
 
 private func generateId(_ prefix: String) -> String {
     prefix + UUID().uuidString
+}
+
+public extension View {
+    /// Cuts out the given shape from the view. This is used instead of a ZStack with a shape and a
+    /// blendMode of .destinationOut because that causes issues on iOS 14 devices
+    public func cutout<S: Shape>(_ shape: S) -> some View {
+        self.clipShape(
+            StackedShape(bottom: Rectangle(), top: shape),
+            style: FillStyle(eoFill: true)
+        )
+    }
+}
+
+private struct StackedShape<Bottom: Shape, Top: Shape>: Shape {
+    var bottom: Bottom
+    var top: Top
+
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.addPath(bottom.path(in: rect))
+            path.addPath(top.path(in: rect))
+        }
+    }
 }
