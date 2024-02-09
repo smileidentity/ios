de9b4847a8428560538506b7cbbb2f676ef9f1fa
Japhet <jndhlovu54@gmail.com>
2024-02-09 16:25:40 +0200

    feat: test precommit hook

--- Example/SmileID/HomeViewModel.swift
+++ Example/SmileID/HomeViewModel.swift
@@ -156,10 +156,12 @@ class HomeViewModel: ObservableObject,
         toastMessage = "Consent Granted"
     }
 
-    func onConsentDenied() {
+    func onConsentDenied() 
+    {
         dismissModal()
         showToast = true
         toastMessage = "Consent Denied"
+                                             print(toastMessage)
     }
 
     private func dismissModal() {
