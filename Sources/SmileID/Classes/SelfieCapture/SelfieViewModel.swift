793928aa04e143c35e51c20874962c996067657b
Juma Allan <allanjuma@gmail.com>
2024-02-14 16:41:11 +0300

    Update SelfieViewModel.swift

--- Sources/SmileID/Classes/SelfieCapture/SelfieViewModel.swift
+++ Sources/SmileID/Classes/SelfieCapture/SelfieViewModel.swift
@@ -186,7 +186,6 @@ public class SelfieViewModel: ObservableObject, ARKitSmileDelegate {
                         ) else {
                             throw SmileIDError.unknown("Error resizing liveness image")
                         }
-//                        let imageUrl = try LocalStorage.saveImage(image: imageData, name: "liveness")
                         let imageUrl = try LocalStorage.createLivenessFile(jobId: jobId, livenessFile: imageData)
                         livenessImages.append(imageUrl)
                         DispatchQueue.main.async {
@@ -201,7 +200,6 @@ public class SelfieViewModel: ObservableObject, ARKitSmileDelegate {
                         ) else {
                             throw SmileIDError.unknown("Error resizing selfie image")
                         }
-//                        let selfieImage = try LocalStorage.saveImage(image: imageData, name: "selfie")
                         let selfieImage = try LocalStorage.createSelfieFile(jobId: jobId, selfieFile: imageData)
                         self.selfieImage = selfieImage
                         DispatchQueue.main.async {
