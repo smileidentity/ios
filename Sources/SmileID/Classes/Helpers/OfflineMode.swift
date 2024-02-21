1c87b3c1e37b97cef369f8ec6e0c70286a8f75ee
Juma Allan <allanjuma@gmail.com>
2024-02-21 18:13:10 +0300

    reworked offline mode submission

--- Sources/SmileID/Classes/Helpers/OfflineMode.swift
+++ Sources/SmileID/Classes/Helpers/OfflineMode.swift
@@ -5,45 +5,23 @@ public class OfflineMode {
     static func submitJob(
         jobId: String,
         deleteFilesOnSuccess: Bool
-    ) throws -> AnyPublisher<UploadResponse, Error> {
-        return try authentication(jobId: jobId)
-            .flatMap { authResponse in
-                do {
-                    return try self.prepUpload(jobId: jobId, authResponse: authResponse)
-                } catch {
-                    return Fail(error: error).eraseToAnyPublisher()
-                }
+    ) {
+        Task {
+            do {
+                let authRequest = try LocalStorage.fetchAuthenticationRequestFile(jobId: jobId)
+                let authResponse = try await SmileID.api.authenticate(
+                    request: authRequest
+                ).async()
+                let prepUploadRequest = try LocalStorage.fetchPrepUploadFile(jobId: jobId)
+                let prepUploadResponse = try await SmileID.api.prepUpload(
+                    request: prepUploadRequest
+                ).async()
+                let zip = try LocalStorage.fetchUploadZip(jobId: jobId)
+                let response = try await SmileID.api.upload(
+                    zip: zip,
+                    to: prepUploadResponse.uploadUrl
+                ).async()
             }
-            .flatMap { prepUploadResponse in
-                do {
-                    return try self.upload(jobId: jobId, prepUploadResponse: prepUploadResponse)
-                } catch {
-                    return Fail(error: error).eraseToAnyPublisher()
-                }
-            }
-            .eraseToAnyPublisher()
-    }
-
-   static private func authentication(
-        jobId: String
-    ) throws -> AnyPublisher<AuthenticationResponse, Error> {
-        let authRequest = try LocalStorage.fetchAuthenticationRequestFile(jobId: jobId)
-        return SmileID.api.authenticate(request: authRequest)
-    }
-
-    static private func prepUpload(
-        jobId: String,
-        authResponse: AuthenticationResponse
-    ) throws -> AnyPublisher<PrepUploadResponse, Error> {
-        let prepUploadRequest = try LocalStorage.fetchPrepUploadFile(jobId: jobId)
-        return SmileID.api.prepUpload(request: prepUploadRequest)
-    }
-
-    static private func upload(
-        jobId: String,
-        prepUploadResponse: PrepUploadResponse
-    ) throws -> AnyPublisher<UploadResponse, Error> {
-        let data = try LocalStorage.fetchUploadZip(jobId: jobId)
-        return SmileID.api.upload(zip: data, to: prepUploadResponse.uploadUrl)
+        }
     }
 }
