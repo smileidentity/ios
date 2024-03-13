a70084978c29edc643e7b4034d5289d8413f9084
JNdhlovu <jndhlovu54@gmail.com>
2024-03-13 16:13:12 +0200

    fix: partner params

--- Sources/SmileID/Classes/Networking/Models/PartnerParams.swift
+++ Sources/SmileID/Classes/Networking/Models/PartnerParams.swift
@@ -5,7 +5,7 @@ public struct PartnerParams: Codable {
     public var userId: String
     public var jobType: JobType?
     public var extras: [String: String]?
-
+    
     public init(
         jobId: String,
         userId: String,
@@ -17,39 +17,54 @@ public struct PartnerParams: Codable {
         self.jobType = jobType
         self.extras = extras
     }
-
+    
     enum CodingKeys: String, CodingKey {
         case jobId = "job_id"
         case userId = "user_id"
         case jobType = "job_type"
         case extras
     }
-
+    
     // Method for copying with modified properties
     func copy(extras: [String: String]?) -> PartnerParams {
         PartnerParams(
             jobId: jobId, userId: userId, jobType: jobType, extras: extras
         )
     }
+}
 
-    var unpackedExtras: [String: String] {
-        var unpacked = [String: String]()
-        unpacked[CodingKeys.jobId.rawValue] = jobId
-        unpacked[CodingKeys.userId.rawValue] = userId
-        if let jobType = jobType {
-            unpacked[CodingKeys.jobType.rawValue] = String(jobType.rawValue)
+extension PartnerParams {
+
+    struct DynamicCodingKey: CodingKey {
+        var stringValue: String
+        var intValue: Int?
+
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+            self.intValue = nil
         }
-        if let extras = extras {
-            for (key, value) in extras {
-                unpacked[key] = value
-            }
+
+        init?(intValue: Int) {
+            self.stringValue = String(intValue)
+            self.intValue = intValue
         }
-        return unpacked
     }
 
     public func encode(to encoder: Encoder) throws {
-        var container = encoder.unkeyedContainer()
-        try container.encode(self.unpackedExtras)
-    }
+        var container = encoder.container(keyedBy: DynamicCodingKey.self)
 
+        // Encode known properties with predefined keys
+        try container.encode(jobId, forKey: .init(stringValue: CodingKeys.jobId.rawValue)!)
+        try container.encode(userId, forKey: .init(stringValue: CodingKeys.userId.rawValue)!)
+        if let jobType = jobType {
+            try container.encode(jobType, forKey: .init(stringValue: CodingKeys.jobType.rawValue)!)
+        }
+
+        // Encode extras directly at the root level
+        if let extras = extras {
+            for (key, value) in extras {
+                try container.encode(value, forKey: .init(stringValue: key)!)
+            }
+        }
+    }
 }
