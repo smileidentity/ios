5016f38f354a7b230b236f1df6d7c17600b957db
Juma Allan <allanjuma@gmail.com>
2024-05-09 13:08:06 +0300

    rebase with main

--- Sources/SmileID/Classes/Networking/Models/v2/JobType.swift .swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public enum JobTypeV2: String, Codable {
-    case smartSelfieAuthentication = "smart_selfie_authentication"
-    case smartSelfieEnrollment = "smart_selfie_enrollment"
-    case unknown = "Unknown"
-}
