5f63c6d17f14b1048a2898cf7e2ed3bf9f531b0e
JNdhlovu <jndhlovu54@gmail.com>
2024-05-14 16:03:37 +0200

    feat: build for spm on every PR

--- Scripts/update_compile_sources.rb
+++ Scripts/update_compile_sources.rb
@@ -1,23 +1,31 @@
 # We use the Example/SmileID.xcworkspace when
-# updating that it may not update the
+# updating that it won't update the
 # SmileID.xcodeproj on the root of the project
+# Which is used for SPM and Carthage
 # this will not affect the Example project
-
 require 'xcodeproj'
 
 project_path = 'SmileID.xcodeproj'
 project = Xcodeproj::Project.open(project_path)
 
-target = project.targets.find { |t| t.name == 'SmileIDFramework' }
-sources_build_phase = target.source_build_phase
+# Update a target with the Swift files
+def update_target(project, target_name)
+  target = project.targets.find { |t| t.name == target_name }
+  return unless target
 
-# Remove all files
-sources_build_phase.clear
+  sources_build_phase = target.source_build_phase
 
-# Add all Swift files from the Sources folder in alphabetical order to avoid
-# having these be different every time Carthage is run and or this is run locally
-Dir.glob(File.join('Sources', '**', '*.swift')).sort.each do |file|
-  sources_build_phase.add_file_reference(project.new_file(file))
+  # Remove all files
+  sources_build_phase.clear
+
+  # Add all Swift files from the Sources folder in alphabetical order
+  Dir.glob(File.join('Sources', '**', '*.swift')).sort.each do |file|
+    sources_build_phase.add_file_reference(project.new_file(file))
+  end
 end
+
+# Update both SmileID and SmileIDFramework targets
+update_target(project, 'SmileID')
+update_target(project, 'SmileIDFramework')
 
 project.save
