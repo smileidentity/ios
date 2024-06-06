429180be1a5a22948a69221bbec15adfa5c74899
Juma Allan <allanjuma@gmail.com>
2024-06-06 18:28:22 +0300

    update error handling ios

--- Sources/SmileID/Classes/Util.swift
+++ Sources/SmileID/Classes/Util.swift
@@ -55,39 +55,10 @@ extension String {
     }
 }
 
-let errorMappings: [Set<String>: String] = [
-    ["2201", "2301", "2401"]: "Si.Error.Message.2201.2301.2401",
-    ["2205", "2405"]: "Si.Error.Message.2205.2405",
-    ["2213", "2413"]: "Si.Error.Message.2213.2413",
-    ["2203"]: "Si.Error.Message.2203",
-    ["2204"]: "Si.Error.Message.2204",
-    ["2207"]: "Si.Error.Message.2207",
-    ["2208"]: "Si.Error.Message.2208",
-    ["2209"]: "Si.Error.Message.2209",
-    ["2210"]: "Si.Error.Message.2210",
-    ["2211"]: "Si.Error.Message.2211",
-    ["2212"]: "Si.Error.Message.2212",
-    ["2215"]: "Si.Error.Message.2215",
-    ["2216"]: "Si.Error.Message.2216",
-    ["2220"]: "Si.Error.Message.2220",
-    ["2221"]: "Si.Error.Message.2221",
-    ["2314"]: "Si.Error.Message.2314",
-    ["2414"]: "Si.Error.Message.2414"
-]
-
-func findErrorMessage(for code: String) -> String {
-    for (codes, message) in errorMappings {
-        if codes.contains(code) {
-            return message
-        }
-    }
-    return "Confirmation.FailureReason"
-}
-
 func toErrorMessage(error: SmileIDError) -> (String, String?) {
     switch error {
     case .api(let code, let message):
-        let errorMessage = findErrorMessage(for: code)
+        let errorMessage = "Si.Error.Message.\(code)"
         return (errorMessage, message)
     default:
         return ("Confirmation.FailureReason", nil)
