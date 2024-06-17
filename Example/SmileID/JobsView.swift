39a2d89e1d00d0ae7aec3ba5b888073a5ace13eb
Tobi Omotayo <tobitech@ymail.com>
2024-06-17 14:56:23 +0100

    change folder structure. add smileid theme background to the jobs screen.

---
+++ Example/SmileID/JobsView.swift
@@ -0,0 +1,37 @@
+import SmileID
+import SwiftUI
+
+struct JobsView: View {
+    var body: some View {
+        NavigationView {
+            let scrollView = ScrollView {
+                VStack(spacing: 0) {
+                    ForEach(0...5, id: \.self) { _ in
+                        JobListItem(job: JobData(title: "Job 1", jobType: .documentVerification))
+                    }
+                    Spacer()
+                }
+            }
+            .background(SmileID.theme.backgroundLight.ignoresSafeArea())
+            .navigationTitle("Jobs")
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {}, label: {
+                        Image(systemName: "trash.fill")
+                    })
+                    .buttonStyle(.plain)
+                }
+            }
+            
+            if #available(iOS 16.0, *) {
+                scrollView.toolbarBackground(SmileID.theme.backgroundLight, for: .navigationBar)
+            } else {
+                scrollView
+            }
+        }
+    }
+}
+
+#Preview {
+    JobsView()
+}
