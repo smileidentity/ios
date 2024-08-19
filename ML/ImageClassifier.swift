3fa357b2e7244915e9ea51de6c9332b4fd254b4a
Tobi Omotayo <tobitech@ymail.com>
2024-08-19 13:59:45 +0100

    Replace the selfie quality model with a new version that takes an image as input.

--- ML/ImageClassifier.swift
+++ ML/ImageClassifier.swift
@@ -28,26 +28,18 @@ class ModelImageClassifier {
     /// Classifies an image using the Core ML Model
     /// - Parameter image: The input image as a UIImage
     /// - Returns: A result containing classifiction confidence.
-    func classify(image: UIImage) async throws -> ImageQualityResult {
+    func classify(imageBuffer: CVPixelBuffer) async throws -> ImageQualityResult {
         do {
-            let mlMultiArray = try await preprocessImage(image)
-            let output = try performClassification(mlMultiArray: mlMultiArray)
-            return output
+            guard let image = UIImage(pixelBuffer: imageBuffer) else {
+                throw ImageClassifierError.preprocessingFailed
+            }
+            let croppedImage = try await cropToFace(image: image)
+            guard let convertedImage = croppedImage.pixelBuffer(width: 120, height: 120) else {
+                throw ImageClassifierError.preprocessingFailed
+            }
+            return try performClassification(imageBuffer: convertedImage)
         } catch {
             throw error
-        }
-    }
-
-    /// Preprocesses the input image to match the model's required input format and size.
-    /// - Parameter image: The originial input image
-    /// - Returns: A MultiArray matching the model's input specifications.
-    private func preprocessImage(_ image: UIImage) async throws -> MLMultiArray {
-        do {
-            let croppedImage = try await cropToFace(image: image)
-            let multiArray = try imageToMLMultiArray(croppedImage)
-            return multiArray
-        } catch {
-            throw ImageClassifierError.preprocessingFailed
         }
     }
 
@@ -89,93 +81,14 @@ class ModelImageClassifier {
         return UIImage(cgImage: croppedImage)
     }
 
-    private func imageToMLMultiArray(_ image: UIImage) throws -> MLMultiArray {
-        let inputSize = CGSize(width: 120, height: 120)
-        UIGraphicsBeginImageContextWithOptions(inputSize, false, 0.0)
-        image.draw(in: CGRect(origin: .zero, size: inputSize))
-        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        guard let resizedImage = resizedImage,
-              let mlMultiArray = toMLMultiArrayInternal(image: resizedImage) else {
-            throw ImageClassifierError.preprocessingFailed
-        }
-        return mlMultiArray
-    }
-
-    private func toMLMultiArrayInternal(image: UIImage) -> MLMultiArray? {
-        guard let cgImage = image.cgImage else {
-            return nil
-        }
-
-        // These values were gotten from the model input specification:
-        // MultiArray (Float32 1 × 120 × 120 × 3)
-        let width = 120
-        let height = 120
-        let channels = 3
-
-        let array = try? MLMultiArray(
-            shape: [
-                1, NSNumber(value: height),
-                NSNumber(value: width),
-                NSNumber(value: channels)],
-            dataType: .float32
-        )
-
-        guard let mlMultiArray = array else {
-            return nil
-        }
-
-        let context = CIContext()
-        let inputImage = CIImage(cgImage: cgImage)
-
-        guard let bitmap = context.createCGImage(inputImage, from: inputImage.extent) else {
-            return nil
-        }
-
-        let pixelBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: height * width * channels)
-        defer { pixelBuffer.deallocate() }
-
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let contextRef = CGContext(
-            data: pixelBuffer,
-            width: width,
-            height: height,
-            bitsPerComponent: 8,
-            bytesPerRow: width * 4,
-            space: colorSpace,
-            bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue
-        )
-
-        contextRef?.draw(bitmap, in: CGRect(x: 0, y: 0, width: width, height: height))
-
-        let pointer = UnsafeMutablePointer<Float32>(OpaquePointer(mlMultiArray.dataPointer))
-
-        for yAxis in 0..<height {
-            for xAxis in 0..<width {
-                let pixelIndex = (yAxis * width * channels) + (xAxis * channels)
-
-                let red = Float32(pixelBuffer[pixelIndex]) / 255.0
-                let green = Float32(pixelBuffer[pixelIndex + 1]) / 255.0
-                let blue = Float32(pixelBuffer[pixelIndex + 2]) / 255.0
-
-                pointer[pixelIndex + 0] = red
-                pointer[pixelIndex + 1] = green
-                pointer[pixelIndex + 2] = blue
-            }
-        }
-
-        return mlMultiArray
-    }
-
     /// Performs the actual classification using the `ImageQualityCP20` model
     /// - Parameter mlMultiArray: The processed input image as a MultiArray
     /// - Returns: The ImageQualityResult
-    private func performClassification(mlMultiArray: MLMultiArray) throws -> ImageQualityResult {
+    private func performClassification(imageBuffer: CVPixelBuffer) throws -> ImageQualityResult {
         let modelConfiguration = MLModelConfiguration()
-        let model = try ImageQualityCP20(configuration: modelConfiguration)
+        let model = try SelfieQualityDetector(configuration: modelConfiguration)
 
-        let input = ImageQualityCP20Input(conv2d_193_input: mlMultiArray)
+        let input = SelfieQualityDetectorInput(conv2d_193_input: imageBuffer)
 
         let prediction = try model.prediction(input: input)
         let output = prediction.Identity
--- ML/SelfieQualityDetector.swift
+++ ML/SelfieQualityDetector.swift
@@ -1,6 +1,5 @@
-// swiftlint:disable all
 //
-// ImageQualityCP20.swift
+// SelfieQualityDetector.swift
 //
 // This file was automatically generated and should not be edited.
 //
@@ -10,10 +9,10 @@ import CoreML
 
 /// Model Prediction Input Type
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-class ImageQualityCP20Input : MLFeatureProvider {
+class SelfieQualityDetectorInput : MLFeatureProvider {
 
-    /// conv2d_193_input as 1 × 120 × 120 × 3 4-dimensional array of floats
-    var conv2d_193_input: MLMultiArray
+    /// conv2d_193_input as color (kCVPixelFormatType_32BGRA) image buffer, 120 pixels wide by 120 pixels high
+    var conv2d_193_input: CVPixelBuffer
 
     var featureNames: Set<String> {
         get {
@@ -23,18 +22,29 @@ class ImageQualityCP20Input : MLFeatureProvider {
     
     func featureValue(for featureName: String) -> MLFeatureValue? {
         if (featureName == "conv2d_193_input") {
-            return MLFeatureValue(multiArray: conv2d_193_input)
+            return MLFeatureValue(pixelBuffer: conv2d_193_input)
         }
         return nil
     }
     
-    init(conv2d_193_input: MLMultiArray) {
+    init(conv2d_193_input: CVPixelBuffer) {
         self.conv2d_193_input = conv2d_193_input
     }
 
-    @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-    convenience init(conv2d_193_input: MLShapedArray<Float>) {
-        self.init(conv2d_193_input: MLMultiArray(conv2d_193_input))
+    convenience init(conv2d_193_inputWith conv2d_193_input: CGImage) throws {
+        self.init(conv2d_193_input: try MLFeatureValue(cgImage: conv2d_193_input, pixelsWide: 120, pixelsHigh: 120, pixelFormatType: kCVPixelFormatType_32ARGB, options: nil).imageBufferValue!)
+    }
+
+    convenience init(conv2d_193_inputAt conv2d_193_input: URL) throws {
+        self.init(conv2d_193_input: try MLFeatureValue(imageAt: conv2d_193_input, pixelsWide: 120, pixelsHigh: 120, pixelFormatType: kCVPixelFormatType_32ARGB, options: nil).imageBufferValue!)
+    }
+
+    func setConv2d_193_input(with conv2d_193_input: CGImage) throws  {
+        self.conv2d_193_input = try MLFeatureValue(cgImage: conv2d_193_input, pixelsWide: 120, pixelsHigh: 120, pixelFormatType: kCVPixelFormatType_32ARGB, options: nil).imageBufferValue!
+    }
+
+    func setConv2d_193_input(with conv2d_193_input: URL) throws  {
+        self.conv2d_193_input = try MLFeatureValue(imageAt: conv2d_193_input, pixelsWide: 120, pixelsHigh: 120, pixelFormatType: kCVPixelFormatType_32ARGB, options: nil).imageBufferValue!
     }
 
 }
@@ -42,7 +52,7 @@ class ImageQualityCP20Input : MLFeatureProvider {
 
 /// Model Prediction Output Type
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-class ImageQualityCP20Output : MLFeatureProvider {
+class SelfieQualityDetectorOutput : MLFeatureProvider {
 
     /// Source provided by CoreML
     private let provider : MLFeatureProvider
@@ -78,20 +88,20 @@ class ImageQualityCP20Output : MLFeatureProvider {
 
 /// Class for model loading and prediction
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-class ImageQualityCP20 {
+class SelfieQualityDetector {
     let model: MLModel
 
     /// URL of model assuming it was installed in the same bundle as this class
     class var urlOfModelInThisBundle : URL {
         let bundle = Bundle(for: self)
-        return bundle.url(forResource: "ImageQualityCP20", withExtension:"mlmodelc")!
+        return bundle.url(forResource: "SelfieQualityDetector", withExtension:"mlmodelc")!
     }
 
     /**
-        Construct ImageQualityCP20 instance with an existing MLModel object.
+        Construct SelfieQualityDetector instance with an existing MLModel object.
 
-        Usually the application does not use this initializer unless it makes a subclass of ImageQualityCP20.
-        Such application may want to use `MLModel(contentsOfURL:configuration:)` and `ImageQualityCP20.urlOfModelInThisBundle` to create a MLModel object to pass-in.
+        Usually the application does not use this initializer unless it makes a subclass of SelfieQualityDetector.
+        Such application may want to use `MLModel(contentsOfURL:configuration:)` and `SelfieQualityDetector.urlOfModelInThisBundle` to create a MLModel object to pass-in.
 
         - parameters:
           - model: MLModel object
@@ -101,7 +111,7 @@ class ImageQualityCP20 {
     }
 
     /**
-        Construct ImageQualityCP20 instance by automatically loading the model from the app's bundle.
+        Construct SelfieQualityDetector instance by automatically loading the model from the app's bundle.
     */
     @available(*, deprecated, message: "Use init(configuration:) instead and handle errors appropriately.")
     convenience init() {
@@ -121,7 +131,7 @@ class ImageQualityCP20 {
     }
 
     /**
-        Construct ImageQualityCP20 instance with explicit path to mlmodelc file
+        Construct SelfieQualityDetector instance with explicit path to mlmodelc file
         - parameters:
            - modelURL: the file url of the model
 
@@ -145,7 +155,7 @@ class ImageQualityCP20 {
     }
 
     /**
-        Construct ImageQualityCP20 instance asynchronously with optional configuration.
+        Construct SelfieQualityDetector instance asynchronously with optional configuration.
 
         Model loading may take time when the model content is not immediately available (e.g. encrypted model). Use this factory method especially when the caller is on the main thread.
 
@@ -154,12 +164,12 @@ class ImageQualityCP20 {
           - handler: the completion handler to be called when the model loading completes successfully or unsuccessfully
     */
     @available(macOS 11.0, iOS 14.0, tvOS 14.0, watchOS 7.0, *)
-    class func load(configuration: MLModelConfiguration = MLModelConfiguration(), completionHandler handler: @escaping (Swift.Result<ImageQualityCP20, Error>) -> Void) {
+    class func load(configuration: MLModelConfiguration = MLModelConfiguration(), completionHandler handler: @escaping (Swift.Result<SelfieQualityDetector, Error>) -> Void) {
         return self.load(contentsOf: self.urlOfModelInThisBundle, configuration: configuration, completionHandler: handler)
     }
 
     /**
-        Construct ImageQualityCP20 instance asynchronously with optional configuration.
+        Construct SelfieQualityDetector instance asynchronously with optional configuration.
 
         Model loading may take time when the model content is not immediately available (e.g. encrypted model). Use this factory method especially when the caller is on the main thread.
 
@@ -167,12 +177,12 @@ class ImageQualityCP20 {
           - configuration: the desired model configuration
     */
     @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-    class func load(configuration: MLModelConfiguration = MLModelConfiguration()) async throws -> ImageQualityCP20 {
+    class func load(configuration: MLModelConfiguration = MLModelConfiguration()) async throws -> SelfieQualityDetector {
         return try await self.load(contentsOf: self.urlOfModelInThisBundle, configuration: configuration)
     }
 
     /**
-        Construct ImageQualityCP20 instance asynchronously with URL of the .mlmodelc directory with optional configuration.
+        Construct SelfieQualityDetector instance asynchronously with URL of the .mlmodelc directory with optional configuration.
 
         Model loading may take time when the model content is not immediately available (e.g. encrypted model). Use this factory method especially when the caller is on the main thread.
 
@@ -182,19 +192,19 @@ class ImageQualityCP20 {
           - handler: the completion handler to be called when the model loading completes successfully or unsuccessfully
     */
     @available(macOS 11.0, iOS 14.0, tvOS 14.0, watchOS 7.0, *)
-    class func load(contentsOf modelURL: URL, configuration: MLModelConfiguration = MLModelConfiguration(), completionHandler handler: @escaping (Swift.Result<ImageQualityCP20, Error>) -> Void) {
+    class func load(contentsOf modelURL: URL, configuration: MLModelConfiguration = MLModelConfiguration(), completionHandler handler: @escaping (Swift.Result<SelfieQualityDetector, Error>) -> Void) {
         MLModel.load(contentsOf: modelURL, configuration: configuration) { result in
             switch result {
             case .failure(let error):
                 handler(.failure(error))
             case .success(let model):
-                handler(.success(ImageQualityCP20(model: model)))
+                handler(.success(SelfieQualityDetector(model: model)))
             }
         }
     }
 
     /**
-        Construct ImageQualityCP20 instance asynchronously with URL of the .mlmodelc directory with optional configuration.
+        Construct SelfieQualityDetector instance asynchronously with URL of the .mlmodelc directory with optional configuration.
 
         Model loading may take time when the model content is not immediately available (e.g. encrypted model). Use this factory method especially when the caller is on the main thread.
 
@@ -203,22 +213,22 @@ class ImageQualityCP20 {
           - configuration: the desired model configuration
     */
     @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-    class func load(contentsOf modelURL: URL, configuration: MLModelConfiguration = MLModelConfiguration()) async throws -> ImageQualityCP20 {
+    class func load(contentsOf modelURL: URL, configuration: MLModelConfiguration = MLModelConfiguration()) async throws -> SelfieQualityDetector {
         let model = try await MLModel.load(contentsOf: modelURL, configuration: configuration)
-        return ImageQualityCP20(model: model)
+        return SelfieQualityDetector(model: model)
     }
 
     /**
         Make a prediction using the structured interface
 
         - parameters:
-           - input: the input to the prediction as ImageQualityCP20Input
+           - input: the input to the prediction as SelfieQualityDetectorInput
 
         - throws: an NSError object that describes the problem
 
-        - returns: the result of the prediction as ImageQualityCP20Output
+        - returns: the result of the prediction as SelfieQualityDetectorOutput
     */
-    func prediction(input: ImageQualityCP20Input) throws -> ImageQualityCP20Output {
+    func prediction(input: SelfieQualityDetectorInput) throws -> SelfieQualityDetectorOutput {
         return try self.prediction(input: input, options: MLPredictionOptions())
     }
 
@@ -226,64 +236,47 @@ class ImageQualityCP20 {
         Make a prediction using the structured interface
 
         - parameters:
-           - input: the input to the prediction as ImageQualityCP20Input
+           - input: the input to the prediction as SelfieQualityDetectorInput
            - options: prediction options 
 
         - throws: an NSError object that describes the problem
 
-        - returns: the result of the prediction as ImageQualityCP20Output
+        - returns: the result of the prediction as SelfieQualityDetectorOutput
     */
-    func prediction(input: ImageQualityCP20Input, options: MLPredictionOptions) throws -> ImageQualityCP20Output {
+    func prediction(input: SelfieQualityDetectorInput, options: MLPredictionOptions) throws -> SelfieQualityDetectorOutput {
         let outFeatures = try model.prediction(from: input, options:options)
-        return ImageQualityCP20Output(features: outFeatures)
+        return SelfieQualityDetectorOutput(features: outFeatures)
     }
 
     /**
         Make an asynchronous prediction using the structured interface
 
         - parameters:
-           - input: the input to the prediction as ImageQualityCP20Input
+           - input: the input to the prediction as SelfieQualityDetectorInput
            - options: prediction options 
 
         - throws: an NSError object that describes the problem
 
-        - returns: the result of the prediction as ImageQualityCP20Output
+        - returns: the result of the prediction as SelfieQualityDetectorOutput
     */
     @available(macOS 14.0, iOS 17.0, tvOS 17.0, watchOS 10.0, *)
-    func prediction(input: ImageQualityCP20Input, options: MLPredictionOptions = MLPredictionOptions()) async throws -> ImageQualityCP20Output {
+    func prediction(input: SelfieQualityDetectorInput, options: MLPredictionOptions = MLPredictionOptions()) async throws -> SelfieQualityDetectorOutput {
         let outFeatures = try await model.prediction(from: input, options:options)
-        return ImageQualityCP20Output(features: outFeatures)
+        return SelfieQualityDetectorOutput(features: outFeatures)
     }
 
     /**
         Make a prediction using the convenience interface
 
         - parameters:
-            - conv2d_193_input as 1 × 120 × 120 × 3 4-dimensional array of floats
+            - conv2d_193_input as color (kCVPixelFormatType_32BGRA) image buffer, 120 pixels wide by 120 pixels high
 
         - throws: an NSError object that describes the problem
 
-        - returns: the result of the prediction as ImageQualityCP20Output
+        - returns: the result of the prediction as SelfieQualityDetectorOutput
     */
-    func prediction(conv2d_193_input: MLMultiArray) throws -> ImageQualityCP20Output {
-        let input_ = ImageQualityCP20Input(conv2d_193_input: conv2d_193_input)
-        return try self.prediction(input: input_)
-    }
-
-    /**
-        Make a prediction using the convenience interface
-
-        - parameters:
-            - conv2d_193_input as 1 × 120 × 120 × 3 4-dimensional array of floats
-
-        - throws: an NSError object that describes the problem
-
-        - returns: the result of the prediction as ImageQualityCP20Output
-    */
-
-    @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-    func prediction(conv2d_193_input: MLShapedArray<Float>) throws -> ImageQualityCP20Output {
-        let input_ = ImageQualityCP20Input(conv2d_193_input: conv2d_193_input)
+    func prediction(conv2d_193_input: CVPixelBuffer) throws -> SelfieQualityDetectorOutput {
+        let input_ = SelfieQualityDetectorInput(conv2d_193_input: conv2d_193_input)
         return try self.prediction(input: input_)
     }
 
@@ -291,21 +284,21 @@ class ImageQualityCP20 {
         Make a batch prediction using the structured interface
 
         - parameters:
-           - inputs: the inputs to the prediction as [ImageQualityCP20Input]
+           - inputs: the inputs to the prediction as [SelfieQualityDetectorInput]
            - options: prediction options 
 
         - throws: an NSError object that describes the problem
 
-        - returns: the result of the prediction as [ImageQualityCP20Output]
+        - returns: the result of the prediction as [SelfieQualityDetectorOutput]
     */
-    func predictions(inputs: [ImageQualityCP20Input], options: MLPredictionOptions = MLPredictionOptions()) throws -> [ImageQualityCP20Output] {
+    func predictions(inputs: [SelfieQualityDetectorInput], options: MLPredictionOptions = MLPredictionOptions()) throws -> [SelfieQualityDetectorOutput] {
         let batchIn = MLArrayBatchProvider(array: inputs)
         let batchOut = try model.predictions(from: batchIn, options: options)
-        var results : [ImageQualityCP20Output] = []
+        var results : [SelfieQualityDetectorOutput] = []
         results.reserveCapacity(inputs.count)
         for i in 0..<batchOut.count {
             let outProvider = batchOut.features(at: i)
-            let result =  ImageQualityCP20Output(features: outProvider)
+            let result =  SelfieQualityDetectorOutput(features: outProvider)
             results.append(result)
         }
         return results
