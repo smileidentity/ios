3534e16bfe351a685568e5a09ba9978141b72202
JNdhlovu <jndhlovu54@gmail.com>
2024-08-28 13:28:04 +0200

    feat: prepupload extension

--- Sources/SmileID/Classes/SmileID.swift
+++ Sources/SmileID/Classes/SmileID.swift
@@ -3,7 +3,7 @@ import SwiftUI
 import UIKit
 
 public class SmileID {
-    public static let version = "10.2.7"
+    public static let version = "10.2.8"
     @Injected var injectedApi: SmileIDServiceable
     public static var configuration: Config { config }
 
