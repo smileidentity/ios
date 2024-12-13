16c491c99780b9fe6c56b6924304839c4bb0a853
Juma Allan <allanjuma@gmail.com>
2024-12-13 19:11:09 +0300

    updated docs

--- Sources/SmileID/Classes/SmileID.swift
+++ Sources/SmileID/Classes/SmileID.swift
@@ -409,17 +409,10 @@ public class SmileID {
     ///   - userId: The user ID to associate with the SmartSelfie™ Enrollment. Most often, this will
     ///     correspond to a unique User ID within your own system. If not provided, a random user ID
     ///     will be generated.
-    ///   - jobId: The job ID to associate with the SmartSelfie™ Enrollment. Most often, this will
-    ///     correspond to a unique Job ID within your own system. If not provided, a random job ID
-    ///     will be generated.
     ///   - allowNewEnroll:  Allows a partner to enroll the same user id again
-    ///   - allowAgentMode: Whether to allow Agent Mode or not. If allowed, a switch will be
-    ///     displayed allowing toggling between the back camera and front camera. If not allowed,
-    ///     only the front camera will be used.
     ///   - showAttribution: Whether to show the Smile ID attribution or not on the Instructions
     ///     screen
     ///   - showInstructions: Whether to deactivate capture screen's instructions for SmartSelfie.
-    ///   - skipApiSubmission: Whether to skip api submission to SmileID and return only captured images
     ///   - extraPartnerParams: Custom values specific to partners
     ///   - delegate: Callback to be invoked when the SmartSelfie™ Authentication is complete.
     @ViewBuilder public class func smartSelfieAuthenticationScreenEnhanced(
