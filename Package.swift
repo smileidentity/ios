9e2d61a9948bc722e23be390ed69da0669fc615f
Tobi Omotayo <tobitech@ymail.com>
2025-01-17 16:00:23 +0100

    add to spm

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0")),
         .package(url: "https://github.com/airbnb/lottie-spm", from: "4.4.2"),
         .package(url: "https://github.com/fingerprintjs/fingerprintjs-ios", from: "1.5.0"),
-        .package(url: "https://github.com/getsentry/sentry-cocoa", exact: "8.36.0")
+        .package(url: "https://github.com/getsentry/sentry-cocoa", exact: "8.36.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.0"))
     ],
     targets: [
         .target(
@@ -25,7 +26,8 @@ let package = Package(
                 .product(name: "ZIPFoundation", package: "ZIPFoundation"),
                 .product(name: "FingerprintJS", package: "fingerprintjs-ios"),
                 .product(name: "Lottie", package: "lottie-spm"),
-                .product(name: "Sentry", package: "sentry-cocoa")
+                .product(name: "Sentry", package: "sentry-cocoa"),
+                .product(name: "Alamofire", package: "Alamofire")
             ],
             path: "Sources/SmileID",
             resources: [.process("Resources")]
