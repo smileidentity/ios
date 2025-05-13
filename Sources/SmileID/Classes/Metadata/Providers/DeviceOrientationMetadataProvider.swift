d2263d350b3efdd260fe5b75d24b1298fd386698
Robin Ankele <robin@smileidentity.com>
2025-05-13 16:52:14 +0100

    move more files

--- Sources/SmileID/Classes/Metadata/Providers/DeviceOrientationMetadataProvider.swift
+++ Sources/SmileID/Classes/Metadata/Providers/DeviceOrientationMetadataProvider.swift
@@ -83,3 +83,22 @@ class DeviceOrientationMetadataProvider: MetadataProvider {
         }
     }
 }
+
+extension UIDeviceOrientation {
+    var category: String {
+        switch self {
+        case .portrait, .portraitUpsideDown:
+            return "Portrait"
+        case .landscapeLeft, .landscapeRight:
+            return "Landscape"
+        case .faceUp:
+            return "FaceUp"
+        case .faceDown:
+            return "FaceDown"
+        case .unknown:
+            return "unknown"
+        @unknown default:
+            return "unknown"
+        }
+    }
+}
