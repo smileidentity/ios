499e5cf0b5e6e09391ab29dd55ce373b3125b658
Robin Ankele <robin@smileidentity.com>
2025-05-13 10:40:42 +0100

    remove unused extension used for alert

--- Sources/SmileID/Classes/Networking/ServiceRunnable.swift
+++ Sources/SmileID/Classes/Networking/ServiceRunnable.swift
@@ -422,32 +422,3 @@ extension ServiceRunnable {
         return body
     }
 }
-
-extension UIApplication {
-    class func getTopViewController(
-        base: UIViewController? = UIWindow.current?.rootViewController
-    ) -> UIViewController? {
-        if let navController = base as? UINavigationController {
-            return getTopViewController(base: navController.visibleViewController)
-        } else if let tabController = base as? UITabBarController,
-            let selected = tabController.selectedViewController
-        {
-            return getTopViewController(base: selected)
-        } else if let presented = base?.presentedViewController {
-            return getTopViewController(base: presented)
-        }
-        return base
-    }
-}
-
-extension UIWindow {
-    static var current: UIWindow? {
-        for scene in UIApplication.shared.connectedScenes {
-            guard let windowScene = scene as? UIWindowScene else { continue }
-            for window in windowScene.windows {
-                if window.isKeyWindow { return window }
-            }
-        }
-        return nil
-    }
-}
