0b8e61f31d1d2c8bea5988b9f0ba465a6516b28b
Juma Allan <allanjuma@gmail.com>
2025-06-20 01:56:57 +0300

    working version of tuist 🚀

--- Project.swift
+++ Project.swift
@@ -6,26 +6,34 @@ let project = Project(
     organizationName: "SmileID",
     targets: [
         .target(
-            name: "SmileID",
+            name: "SmileID-Sample",
             destinations: .iOS,
             product: .app,
             bundleId: "com.smileidentity.example-ios",
+            deploymentTargets: .iOS("14.0"),
             sources: ["Example/SmileID/**"],
-            resources: ["Example/SmileID/Resources**"],
-             dependencies: [
-               .target(name: "sdk"),
-            ]
+            resources: ["Example/SmileID/Resources/**"],
+            dependencies: [
+               .target(name: "SmileID")
+            ],
+            settings: .settings(
+                base: [
+                    "DEVELOPMENT_TEAM": "99P7YGX9Q6",
+                    "CODE_SIGN_STYLE": "Automatic",
+                    "CODE_SIGN_IDENTITY": "SmileID",
+                ]
+            ),
         ),
         .target(
-            name: "SmileIDTests",
+            name: "SmileIDExampleTests",
             destinations: .iOS,
             product: .unitTests,
             bundleId: "com.smileidentity.example-ios",
             infoPlist: .default,
             sources: ["Tests/**"],
             resources: [],
-            dependencies: [.target(name: "SmileID")]
+            dependencies: [.target(name: "SmileID-Sample")]
         ),
-       sdk
+        SmileID
     ]
 )
