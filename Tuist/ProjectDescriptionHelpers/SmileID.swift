843e4964577c88bc984b1443a0a01a542287978c
Juma Allan <allanjuma@gmail.com>
2025-06-20 01:56:57 +0300

    working version of tuist 🚀

--- Tuist/ProjectDescriptionHelpers/SmileID.swift
+++ Tuist/ProjectDescriptionHelpers/SmileID.swift
@@ -1,12 +1,14 @@
 import ProjectDescription
 
-public let sdk = Target.target(
-    name: "sdk",
+public let SmileID = Target.target(
+    name: "SmileID",
     destinations: .iOS,
     product: .framework,
     bundleId: "com.smileidentity.sdk",
-    sources: ["Sources/Smile/Classes/"],
-//    resources: ["Sources/Smile/Resources/"],
+    deploymentTargets: .iOS("13.0"),
+    infoPlist: .default,
+    sources: ["Sources/SmileID/Classes/**"],
+    resources: ["Sources/Smile/Resources/**"],
     dependencies: [
         .external(name: "ZIPFoundation"),
         .external(name: "FingerprintJS"),
