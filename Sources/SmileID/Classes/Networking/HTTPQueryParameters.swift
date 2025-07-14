c34e30742db2d226e56788e7f4f77d5413a3c5ab
Tobi Omotayo <tobitech@ymail.com>
2025-07-14 23:57:32 +0100

    Fix retroactive extension declaration for HTTPQueryParameters to improve clarity

--- Sources/SmileID/Classes/Networking/HTTPQueryParameters.swift
+++ Sources/SmileID/Classes/Networking/HTTPQueryParameters.swift
@@ -15,7 +15,7 @@ public struct HTTPQueryParameters: Equatable {
   }
 }
 
-extension [HTTPQueryParameters]: @retroactive ExpressibleByDictionaryLiteral {
+extension [HTTPQueryParameters]: ExpressibleByDictionaryLiteral {
   public init(dictionaryLiteral elements: (String, [String])...) {
     self = elements.map {
       HTTPQueryParameters(key: $0.0, values: $0.1)
