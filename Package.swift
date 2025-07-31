1a974c232ff80fc1d90708a17fb8731868d20ea6
Tobi Omotayo <tobitech@ymail.com>
2025-07-31 12:59:18 +0100

    restore SmileIDSecurity package dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     .package(url: "https://github.com/weichsel/ZIPFoundation.git", exact: "0.9.19"),
     .package(url: "https://github.com/airbnb/lottie-spm", exact: "4.5.2"),
     .package(url: "https://github.com/fingerprintjs/fingerprintjs-ios", exact: "1.6.0"),
-//    .package(url: "https://github.com/smileidentity/smile-id-security", exact: "11.1.0"),
+    .package(url: "https://github.com/smileidentity/smile-id-security", exact: "11.1.0"),
     .package(url: "https://github.com/getsentry/sentry-cocoa", exact: "8.53.2")
   ],
   targets: [
@@ -25,7 +25,7 @@ let package = Package(
         .product(name: "ZIPFoundation", package: "ZIPFoundation"),
         .product(name: "FingerprintJS", package: "fingerprintjs-ios"),
         .product(name: "Lottie", package: "lottie-spm"),
-//        .product(name: "SmileIDSecurity", package: "smile-id-security"),
+        .product(name: "SmileIDSecurity", package: "smile-id-security"),
         .product(name: "Sentry", package: "sentry-cocoa")
       ],
       path: "Sources",
