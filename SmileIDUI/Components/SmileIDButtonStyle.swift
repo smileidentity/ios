7b9a609ddee3c13d4fff156ab0bf698c038502fb
Tobi Omotayo <tobitech@ymail.com>
2025-08-07 16:04:25 +0100

    Refactor SmileIDInstructionsScreen and SmileIDButtonStyle for improved readability

--- SmileIDUI/Components/SmileIDButtonStyle.swift
+++ SmileIDUI/Components/SmileIDButtonStyle.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 
 struct SmileIDButtonStyle: ButtonStyle {
-	func makeBody(configuration: Configuration) -> some View {
-		configuration.label
-			.frame(maxWidth: .infinity)
-			.padding()
-			.background(Color.blue)
-			.foregroundColor(.white)
-			.clipShape(Capsule())
-			.scaleEffect(configuration.isPressed ? 0.9 : 1)
-			.animation(.easeOut(duration: 0.2), value: configuration.isPressed)
-	}
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .frame(maxWidth: .infinity)
+      .padding()
+      .background(Color.blue)
+      .foregroundColor(.white)
+      .clipShape(Capsule())
+      .scaleEffect(configuration.isPressed ? 0.9 : 1)
+      .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+  }
 }
--- SmileIDUI/SmileIDInstructionsScreen.swift
+++ SmileIDUI/SmileIDInstructionsScreen.swift
@@ -1,57 +1,56 @@
 import SwiftUI
 
 struct SmileIDInstructionsScreen<ContinueButtonStyle: ButtonStyle, CancelButtonStyle: ButtonStyle>: View {
+  var continueButtonStyle: ContinueButtonStyle
+  var cancelButtonStyle: CancelButtonStyle
 
-	var onContinue: () -> Void
-	var onCancel: () -> Void
-	var continueButtonStyle: ContinueButtonStyle
-	var cancelButtonStyle: CancelButtonStyle
+  var onContinue: () -> Void
+  var onCancel: () -> Void
 
-	init(
-		onContinue: @escaping () -> Void,
-		onCancel: @escaping () -> Void,
-		continueButtonStyle: ContinueButtonStyle = SmileIDButtonStyle(),
-		cancelButtonStyle: CancelButtonStyle = SmileIDButtonStyle()
-	) {
-		self.onContinue = onContinue
-		self.onCancel = onCancel
-		self.continueButtonStyle = continueButtonStyle
-		self.cancelButtonStyle = cancelButtonStyle
-	}
+  init(
+    onContinue: @escaping () -> Void,
+    onCancel: @escaping () -> Void,
+    continueButtonStyle: ContinueButtonStyle = SmileIDButtonStyle(),
+    cancelButtonStyle: CancelButtonStyle = SmileIDButtonStyle()
+  ) {
+    self.onContinue = onContinue
+    self.onCancel = onCancel
+    self.continueButtonStyle = continueButtonStyle
+    self.cancelButtonStyle = cancelButtonStyle
+  }
 
-	var body: some View {
-		VStack {
-			ScrollView(.vertical) {
+  var body: some View {
+    VStack {
+      ScrollView(.vertical) {
+        Text("Some header here")
+          .font(.title)
+        Text("Some subtitle here")
+        Spacer()
+      }
 
-				Text("Some header here")
-					.font(.title)
-				Text("Some subtitle here")
-				Spacer()
-			}
+      VStack {
+        Button("Continue") {
+          onContinue()
+        }
+        .buttonStyle(continueButtonStyle)
 
-			VStack {
-				Button("Continue") {
-					onContinue()
-				}
-				.buttonStyle(continueButtonStyle)
-
-				Button("Cancel") {
-					onCancel()
-				}
-				.buttonStyle(cancelButtonStyle)
-			}
-			.padding()
-		}
-	}
+        Button("Cancel") {
+          onCancel()
+        }
+        .buttonStyle(cancelButtonStyle)
+      }
+      .padding()
+    }
+  }
 }
 
 #if DEBUG
-#Preview {
-	NavigationView {
-		SmileIDInstructionsScreen(
-			onContinue: {},
-			onCancel: {}
-		)
-	}
-}
+  #Preview {
+    NavigationView {
+      SmileIDInstructionsScreen(
+        onContinue: {},
+        onCancel: {}
+      )
+    }
+  }
 #endif
