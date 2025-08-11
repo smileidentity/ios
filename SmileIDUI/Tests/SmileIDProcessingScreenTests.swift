c30f2ae38363f877f2498b3f26df06a0f55edac0
Tobi Omotayo <tobitech@ymail.com>
2025-08-11 10:09:25 +0100

    Add unit tests for SmileIDProcessingScreen button functionality

---
+++ SmileIDUI/Tests/SmileIDProcessingScreenTests.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+import XCTest
+
+@testable import SmileIDUI
+
+class SmileIDProcessingScreenTests: XCTestCase {
+	func testContinueButtonIsClickable() {
+		var continueCallCount = 0
+
+		let screen = SmileIDProcessingScreen(
+			onContinue: { continueCallCount += 1 },
+			onCancel: {}
+		)
+
+		screen.onContinue()
+
+		XCTAssertEqual(continueCallCount, 1)
+	}
+
+	func testCancelButtonIsClickable() {
+		var cancelCallCount = 0
+
+		let screen = SmileIDProcessingScreen(
+			onContinue: {},
+			onCancel: { cancelCallCount += 1 }
+		)
+
+		screen.onCancel()
+
+		XCTAssertEqual(cancelCallCount, 1)
+	}
+}
