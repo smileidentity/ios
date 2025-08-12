4f69bbd2f51dc73730626869ff7a5f2cf7333798
Tobi Omotayo <tobitech@ymail.com>
2025-08-12 16:59:35 +0100

    Add Backport and StateObject helpers for SwiftUI compatibility

---
+++ SmileIDNavigation/Classes/VerificationFlowView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  SmileID
+//
+//  Created by Oluwatobi Omotayo on 12/08/2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
