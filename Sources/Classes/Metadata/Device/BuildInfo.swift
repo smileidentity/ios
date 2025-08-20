91e3bbf3f67d013a43e9347fd68c75a9d76386f4
Robin Ankele <robin@smileidentity.com>
2025-08-20 18:20:43 +0100

    flatten build info

--- Sources/Classes/Metadata/Device/BuildInfo.swift
+++ Sources/Classes/Metadata/Device/BuildInfo.swift
@@ -1,34 +1,24 @@
 import Foundation
 import StoreKit
 
-struct BuildInfo {
-  var platform: String
-  var buildSource: String
-
-  func toCodableObject() -> [String: CodableValue] {
-    [
-      "platform": .string(platform),
-      "build_source": .string(buildSource)
-    ]
-  }
-}
-
-func getBuildInfo() async -> BuildInfo {
-  var platform = Platform.iphone.rawValue
+func getBuildPlatform() -> String {
   let info = ProcessInfo.processInfo
   if info.isMacCatalystApp {
-    platform = Platform.mac.rawValue
+    return Platform.mac.rawValue
   }
   if #available(iOS 14.0, *) {
     if info.isiOSAppOnMac {
-      platform = Platform.mac.rawValue
+      return Platform.mac.rawValue
     }
   }
   if info.environment["SIMULATOR_MODEL_IDENTIFIER"] != nil {
-    platform = Platform.simulator.rawValue
+    return Platform.simulator.rawValue
   }
+  return Platform.iphone.rawValue
+}
 
-  let buildSource: String = await {
+func getBuildReceipt() async -> String {
+  await {
     if #available(iOS 16.0, *) {
       do {
         let result = try await AppTransaction.shared
@@ -43,10 +33,4 @@ func getBuildInfo() async -> BuildInfo {
       return "unknown"
     }
   }()
-
-  let buildInfo = BuildInfo(
-    platform: platform,
-    buildSource: buildSource
-  )
-  return buildInfo
 }
