b3e0728eaed0babcebaae495c0f0c7b6cbde9b6e
Tobi Omotayo <tobitech@ymail.com>
2025-08-27 15:16:08 +0100

    formatting.

--- SmileIDNetworking/Classes/Core/DefaultRequestBuilder.swift
+++ SmileIDNetworking/Classes/Core/DefaultRequestBuilder.swift
@@ -1,76 +1,76 @@
 import Foundation
 
 final class DefaultRequestBuilder {
-	private let jsonEncoder: JSONEncoder
-	
-	init(jsonEncoder: JSONEncoder) {
-		self.jsonEncoder = jsonEncoder
-		self.jsonEncoder.dateEncodingStrategy = .iso8601
-	}
-	
-	func makeURLRequest<E: Endpoint>(
-		baseURL: URL,
-		endpoint: E,
-		config: NetworkConfig
-	) throws -> URLRequest {
-		var url = baseURL.appendingPathComponent(endpoint.path)
-		if let query = endpoint.query,
-				!query.isEmpty {
-			var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
-			components?.queryItems = query.map {
-				URLQueryItem(name: $0.key, value: $0.value)
-			}
-			url = components?.url ?? url
-		}
-		
-		var request = URLRequest(url: url)
-		request.httpMethod = endpoint.method.rawValue
-		request.timeoutInterval = config.requestTimeout
-		request.cachePolicy = config.cachePolicy
-		
-		// Apply configured headers first, then endpoint overrides
-		for (key, value) in config.additionalHeaders {
-			request.setValue(value, forHTTPHeaderField: key)
-		}
+  private let jsonEncoder: JSONEncoder
 
-		if let headers = endpoint.headers {
-			for (key, value) in headers {
-				request.setValue(value, forHTTPHeaderField: key)
-			}
-		}
-		
-		if let body = endpoint.body {
-			switch body {
-			case let .json(encodable):
-				let data = try jsonEncoder.encode(AnyEncodable(encodable))
-				request.httpBody = data
-				request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-			case let .formURLEncoded(params):
-				let bodyString = params.map {
-					"\($0.key.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? $0.key)=\($0.value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? $0.value)"
-				}.joined(separator: "&")
-				request.httpBody = bodyString.data(using: .utf8)
-				request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
-			case let .multipart(form):
-				let boundary = form.boundary
-				request.httpBody = form.buildBody()
-				request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-			}
-		}
+  init(jsonEncoder: JSONEncoder) {
+    self.jsonEncoder = jsonEncoder
+    self.jsonEncoder.dateEncodingStrategy = .iso8601
+  }
 
-		return request
-	}
+  func makeURLRequest(
+    baseURL: URL,
+    endpoint: some Endpoint,
+    config: NetworkConfig
+  ) throws -> URLRequest {
+    var url = baseURL.appendingPathComponent(endpoint.path)
+    if let query = endpoint.query,
+       !query.isEmpty {
+      var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+      components?.queryItems = query.map {
+        URLQueryItem(name: $0.key, value: $0.value)
+      }
+      url = components?.url ?? url
+    }
+
+    var request = URLRequest(url: url)
+    request.httpMethod = endpoint.method.rawValue
+    request.timeoutInterval = config.requestTimeout
+    request.cachePolicy = config.cachePolicy
+
+    // Apply configured headers first, then endpoint overrides
+    for (key, value) in config.additionalHeaders {
+      request.setValue(value, forHTTPHeaderField: key)
+    }
+
+    if let headers = endpoint.headers {
+      for (key, value) in headers {
+        request.setValue(value, forHTTPHeaderField: key)
+      }
+    }
+
+    if let body = endpoint.body {
+      switch body {
+      case .json(let encodable):
+        let data = try jsonEncoder.encode(AnyEncodable(encodable))
+        request.httpBody = data
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+      case .formURLEncoded(let params):
+        let bodyString = params.map {
+          "\($0.key.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? $0.key)=\($0.value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? $0.value)"
+        }.joined(separator: "&")
+        request.httpBody = bodyString.data(using: .utf8)
+        request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+      case .multipart(let form):
+        let boundary = form.boundary
+        request.httpBody = form.buildBody()
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+      }
+    }
+
+    return request
+  }
 }
 
 /// Helper to encode `Encodable` without concrete type at callsite.
 private struct AnyEncodable: Encodable {
-	private let encodeClosure: (Encoder) throws -> Void
+  private let encodeClosure: (Encoder) throws -> Void
 
-	init(_ encodable: Encodable) {
-		self.encodeClosure = encodable.encode
-	}
+  init(_ encodable: Encodable) {
+    self.encodeClosure = encodable.encode
+  }
 
-	func encode(to encoder: Encoder) throws {
-		try encodeClosure(encoder)
-	}
+  func encode(to encoder: Encoder) throws {
+    try encodeClosure(encoder)
+  }
 }
