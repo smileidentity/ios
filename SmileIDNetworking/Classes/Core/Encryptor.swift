11a0ce1df6c16ce1c9c6862689234f70278a18c0
Tobi Omotayo <tobitech@ymail.com>
2025-08-28 11:58:23 +0100

    introduce concrete implementation for signer and encryptor.

--- SmileIDNetworking/Classes/Core/Encryptor.swift
+++ SmileIDNetworking/Classes/Core/Encryptor.swift
@@ -1,7 +1,19 @@
+import CryptoKit
 import Foundation
 
 protocol Encryptor {
   func encrypt(
     _ data: Data
   ) throws -> (ciphertext: Data, iv: Data, tag: Data)
+}
+
+// TODO: Replace with encryptor from SmileIDSecurity Package.
+public final class AESGCMEncryptor: Encryptor {
+  private let key: SymmetricKey
+  public init(key: Data) { self.key = SymmetricKey(data: key) }
+  public func encrypt(_ data: Data) throws -> (ciphertext: Data, iv: Data, tag: Data) {
+    let nonce = try AES.GCM.Nonce()
+    let sealed = try AES.GCM.seal(data, using: key, nonce: nonce)
+    return (sealed.ciphertext, Data(nonce), sealed.tag)
+  }
 }
