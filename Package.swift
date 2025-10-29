b40dedd8a4ccee20ac39b92ed5b698f6e7f7c95c
Tobi Omotayo <tobitech@ymail.com>
2025-10-29 13:58:01 +0100

    formatting.

--- Package.swift
+++ Package.swift
@@ -3,40 +3,40 @@
 import PackageDescription
 
 let package = Package(
-	name: "SmileID",
-	defaultLocalization: "en",
-	platforms: [.iOS(.v13)],
-	products: [
-		.library(
-			name: "SmileID",
-			targets: ["SmileID"]
-		)
-	],
-	dependencies: [
-		.package(url: "https://github.com/weichsel/ZIPFoundation.git", exact: "0.9.20"),
-		.package(url: "https://github.com/airbnb/lottie-spm", exact: "4.5.2"),
-		.package(url: "https://github.com/fingerprintjs/fingerprintjs-ios", exact: "1.6.0"),
-		.package(url: "https://github.com/smileidentity/smile-id-security", exact: "11.1.1"),
-		.package(url: "https://github.com/getsentry/sentry-cocoa", exact: "8.57.0")
-	],
-	targets: [
-		.target(
-			name: "SmileID",
-			dependencies: [
-				"SmileIDSDK",
-				.product(name: "ZIPFoundation", package: "ZIPFoundation"),
-				.product(name: "Lottie", package: "lottie-spm"),
-				.product(name: "FingerprintJS", package: "fingerprintjs-ios"),
-				.product(name: "Sentry", package: "sentry-cocoa"),
-				.product(name: "SmileIDSecurity", package: "smile-id-security")
-			],
-			path: "Sources",
-			sources: ["Classes"]
-		),
-		.binaryTarget(
-			name: "SmileIDSDK",
-			url: "...",
-			checksum: "..."
-		)
-	]
+  name: "SmileID",
+  defaultLocalization: "en",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(
+      name: "SmileID",
+      targets: ["SmileID"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/weichsel/ZIPFoundation.git", exact: "0.9.20"),
+    .package(url: "https://github.com/airbnb/lottie-spm", exact: "4.5.2"),
+    .package(url: "https://github.com/fingerprintjs/fingerprintjs-ios", exact: "1.6.0"),
+    .package(url: "https://github.com/smileidentity/smile-id-security", exact: "11.1.1"),
+    .package(url: "https://github.com/getsentry/sentry-cocoa", exact: "8.57.0")
+  ],
+  targets: [
+    .target(
+      name: "SmileID",
+      dependencies: [
+        "SmileIDSDK",
+        .product(name: "ZIPFoundation", package: "ZIPFoundation"),
+        .product(name: "Lottie", package: "lottie-spm"),
+        .product(name: "FingerprintJS", package: "fingerprintjs-ios"),
+        .product(name: "Sentry", package: "sentry-cocoa"),
+        .product(name: "SmileIDSecurity", package: "smile-id-security")
+      ],
+      path: "Sources",
+      sources: ["Classes"]
+    ),
+    .binaryTarget(
+      name: "SmileIDSDK",
+      url: "...",
+      checksum: "..."
+    )
+  ]
 )
